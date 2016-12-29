b8346899e8d50712eb383e68e39b82fd73bc096d
Tanja Hennis <tmhennis@gmail.com>
2016-12-29 14:39:00 +0100

    added seeds

--- app/controllers/prothesis_details_controller.rb
+++ app/controllers/prothesis_details_controller.rb
@@ -1,2 +1,42 @@
 class ProthesisDetailsController < ApplicationController
+
+  def new
+    @prothesis_detail = ProthesisDetail.new
+  end
+
+  def create
+    @prothesis_detail = ProthesisDetail.new(prothesis_detail_params)
+  end
+
+  private
+
+  def prothesis_detail_params
+    params.require(:prothesis_detail).permit(
+      :prothesis,
+      :prothesis_by,
+      :prothesis_date,
+      :indication,
+      :cup_approach,
+      :cup_brand,
+      :cup_cemented,
+      :cup_is_cemented,
+      :cup_not_cemented,
+      :cup_size,
+      :cup_liner,
+      :bone_quality,
+      :bone_paprosky_grade,
+      :bone_pelvic_discontinuity,
+      :stem_cemented,
+      :stem_is_cemented,
+      :stem_not_cemented,
+      :stem_fixation,
+      :stem_brand,
+      :stem_size,
+      :stem_length,
+      :taper,
+      :head,
+      :head_size
+      )
+  end
+
 end
